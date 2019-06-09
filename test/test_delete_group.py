1932f98bcd94cdaa7679b259a7f160fc2f51363b
Sergey Perepelov <pvserge1@gmail.com>
2019-06-09 19:19:18 +0300

    Get list after preconditions

--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -2,9 +2,9 @@ from random import randrange
 
 
 def test_delete_random_group(app):
-    old_list = app.groups.get_group_list()
-    if len(old_list) < 2:
+    if len(app.groups.get_group_list()) < 2:
         app.groups.add_new_group("my group")
+    old_list = app.groups.get_group_list()
     group_index = randrange(1, len(old_list))
     app.groups.delete_group_by_index(group_index)
     new_list = app.groups.get_group_list()
